f1bacb6dd1f8adfa0f4d71cf9ead314283526ed8
davikenji <145460400+davikenji@users.noreply.github.com>
2024-11-03 18:16:30 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma liguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris' 
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'   
+)
+
+criaCartao(
+    'Língua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês é Hi (RAI)'
+)
